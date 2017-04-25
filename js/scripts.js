2fcb838de9b46d8d6de63e37af204e9bf566e230
Kirsten Knapp and Michelle Poterek <kirsten+michelle@>
2017-04-25 10:25:14 -0700

    Add some toggle jquery, additional html and some more css.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
-$(document).ready(function() {
-  $("p").click(function() {
-    $("img").show();
+$(function() {
+
+
+// $("h1").click(function() {
+//   alert("This is a header.");
+// });
+
+  $(".clickable").click(function() {
+    $(".color-showing").toggle();
+    $(".color-hidden").toggle();
   });
+
+
 });
