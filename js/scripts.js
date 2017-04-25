22ff0f21787c397156968bce30270652ac74f1ee
Kirsten Knapp and Michelle Poterek <kirsten+michelle@>
2017-04-25 14:32:35 -0700

    Add buttons with options for light and dark view with jQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,40 @@ $(function() {
 //   alert("This is a header.");
 // });
 
-  $(".color-control").click(function() {
-    $("#color-showing").toggle();
-    $("#color-hidden").toggle();
+$("button#black").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
   });
 
-  $(".flower").click(function() {
-    $("#flower-fade-in").slideUp();
-    $("#flower-fade-out").slideDown();
-
+  $("button#white").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
   });
 
-  $(".flower-control").click(function() {
-    $("#flower-fade-in").slideDown();
-    $("#flower-fade-out").slideUp();
 
-  });
+
+  //
+  //
+  //
+  //
+  //
+  //
+  // $(".color-control").click(function() {
+  //   $("#color-showing").toggle();
+  //   $("#color-hidden").toggle();
+  // });
+  //
+  // $(".flower").click(function() {
+  //   $("#flower-fade-in").slideUp();
+  //   $("#flower-fade-out").slideDown();
+  //
+  // });
+  //
+  // $(".flower-control").click(function() {
+  //   $("#flower-fade-in").slideDown();
+  //   $("#flower-fade-out").slideUp();
+  //
+  // });
 
 
 });
