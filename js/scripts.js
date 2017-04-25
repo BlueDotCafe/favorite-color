3f4309ea9690ccd2595bf3eba50fce89c5f9adf3
Kirsten Knapp and Michelle Poterek <kirsten+michelle@>
2017-04-25 11:45:05 -0700

    Add changes to jquery and html and one more image.

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,21 @@ $(function() {
 //   alert("This is a header.");
 // });
 
-  $(".clickable").click(function() {
-    $(".color-showing").toggle();
-    $(".color-hidden").toggle();
+  $(".color-control").click(function() {
+    $("#color-showing").toggle();
+    $("#color-hidden").toggle();
+  });
+
+  $(".flower").click(function() {
+    $("#flower-fade-in").slideUp();
+    $("#flower-fade-out").slideDown();
+
+  });
+
+  $(".flower-control").click(function() {
+    $("#flower-fade-in").slideDown();
+    $("#flower-fade-out").slideUp();
+
   });
 
 
